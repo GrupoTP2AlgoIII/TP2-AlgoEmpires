acedf3e62f95e64e357f20e6a82219ff260e0bd9
tomas <tallo@fi.uba.ar>
2018-11-09 22:21:48 -0300

    Borro comentarios innecesarios de Mapa

--- src/modelo/mapa/Mapa.java
+++ src/modelo/mapa/Mapa.java
@@ -1,26 +1,8 @@
 package modelo;
 
-//import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
- * Ejemplo
- * 
-  import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-....
-
-Map<String,String> mMap = new HashMap<String, String>();
-mMap.put("key", "something");
-Iterator iter = mMap.entrySet().iterator();
-while (iter.hasNext()) {
-    Map.Entry mEntry = (Map.Entry) iter.next();
-    System.out.println(mEntry.getKey() + " : " + mEntry.getValue());
-}
- */
 
 public class Mapa {
 	
