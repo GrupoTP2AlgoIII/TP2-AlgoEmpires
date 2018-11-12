24dac83fa336b11c3a750a91ebd366949a282de2
tomas <tallo@fi.uba.ar>
2018-11-12 20:34:08 -0300

    redefini los metodos hashcode e equals, despues  agrego pruebas

--- src/modelo/mapa/Posicion.java
+++ src/modelo/mapa/Posicion.java
@@ -90,4 +90,42 @@ public class Posicion {
 		return (x > filas) || (x < 1) || (y > columnas) || (y < 1);
 	}
 
+    // Fuente para redefinir los metodos equals y hashcode:
+	// https://www.geeksforgeeks.org/override-equalsobject-hashcode-method/
+	@Override
+	public boolean equals(Object objeto)
+	{
+
+	// si las dos referencias
+	// se refieren al mismo objeto.
+	if(this == objeto) {
+		return true;
+	}
+
+	// chequea si el argumento es de tipo
+	// Posicion comparando entre las clases
+	// del argumento y de este objeto.
+	// if(!(objeto instanceof Posicion)) return false; > evitar hacer esto.
+	if(objeto == null || objeto.getClass()!= this.getClass()) {
+		return false;
+	}
+
+	// casteo del tipo de dato del argumento.
+	Posicion posicion = (Posicion) objeto;
+
+	// comparacion entre el estado del argumento
+	// y el estado del objeto 'this'.
+	return (posicion.x == this.x && posicion.y == this.y);
+	}
+	
+	@Override
+	public int hashCode(){
+		
+		int resultado = 17;
+		resultado = resultado * (this.x) + (this.y);
+		return resultado;		
+	}
+
+
+
 }
